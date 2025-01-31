ba1eb25fc19eade4a25652f394c30377eddbbacb
Paul Mullen <paullmullen@gmail.com>
2025-01-31 09:52:23 -0600

    getPatientData updated to cloud function for stats page

--- src/helpers/fetchPatientsData.js
+++ src/helpers/fetchPatientsData.js
@@ -1,25 +1,44 @@
-const fetchPatientsData = async (dateRange) => {
-  const url =
-    "https://us-central1-alfarero-478ad.cloudfunctions.net/fetchPatientsData";
+const { onRequest } = require("firebase-functions/v2/https");
+const admin = require("firebase-admin");
 
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ dateRange }),
-    });
+admin.initializeApp();
 
-    if (!response.ok) {
-      throw new Error(`Error fetching data: ${response.statusText}`);
+exports.fetchPatientsData = onRequest(
+  {
+    cors: [/localhost(:\d+)?$/, "http://multimedica.org"],
+    methods: ["GET", "POST", "OPTIONS"], // Allowed methods
+  },
+  async (req, res) => {
+    if (req.method !== "POST") {
+      return res.status(405).send("Method Not Allowed");
     }
 
-    const data = await response.json();
-    return data.patientsData;
-  } catch (error) {
-    console.error("Error fetching patient data:", error);
-    throw error;
+    try {
+      const { dateRange } = req.body;
+
+      // Firestore query logic
+      const db = admin.firestore();
+      const patientsCollection = db.collection("patients");
+      const snapshot = await patientsCollection.get();
+
+      const patientsData = [];
+      snapshot.forEach((doc) => {
+        const data = doc.data();
+        const start_time = data.start_time.toMillis();
+        if (start_time >= dateRange[0] && start_time <= dateRange[1]) {
+          patientsData.push({
+            pt_no: data.pt_no,
+            patient_name: data.patient_name,
+            start_time: data.start_time.toDate(),
+            reason_for_visit: data.reason_for_visit,
+          });
+        }
+      });
+
+      return res.status(200).json({ patientsData });
+    } catch (error) {
+      console.error("Error fetching patients data:", error);
+      return res.status(500).send("Internal Server Error");
+    }
   }
-};
-export { fetchPatientsData };
+);
