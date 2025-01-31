9625d97ba4369c34cd418f5e739a0c69b918e036
Paul Mullen <paulmullen@wi.rr.com>
2025-01-31 09:58:10 -0600

    Refactor-stat-page (#3)
    * get pateint data converted to cloud function
    
    * working getPatients
    
    * getPatientData updated to cloud function for stats page
    
    ---------
    
    Co-authored-by: Paul Mullen <paullmullen@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,10 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 
 // Import your functions from their respective files
-const { aggregateTimes } = require("./aggregateTimes");
+// const { aggregateTimes } = require("./aggregateTimes");
 const { getPatientCount } = require("./getPatientCount");
 const { updateStatusChange } = require("./updateStatusChange");
+const { getPatientsData } = require("./getPatientsData");
 
 admin.initializeApp();
 
@@ -12,3 +13,4 @@ admin.initializeApp();
 exports.aggregateTimes = aggregateTimes;
 exports.getPatientCount = getPatientCount;
 exports.updateStatusChange = updateStatusChange;
+exports.getPatientsData = this.getPatientsData;
