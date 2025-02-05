f3c9e564c9d99124c2d3585c428ea4d1927f7ebe
Paul Mullen <paullmullen@gmail.com>
2025-02-05 09:51:52 -0600

    updated code to use react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Updated import for React 18
 import "./index.css";
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
@@ -16,9 +16,10 @@ i18next.init({
   },
 });
 
-ReactDOM.render(
+// Create a root and render the app
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <I18nextProvider i18n={i18next}>
     <AlfareroApp />
-  </I18nextProvider>,
-  document.getElementById("root")
+  </I18nextProvider>
 );
