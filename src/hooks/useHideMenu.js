edf7ec54db41490e79a1816a45be2edab62609b0
Paul Mullen <paullmullen@gmail.com>
2025-02-12 09:40:17 -0600

    cleared out a small warning in Turno.js

--- src/hooks/useHideMenu.js
+++ src/hooks/useHideMenu.js
@@ -1,18 +1,14 @@
-import { useContext, useEffect } from 'react'
-import { UiContext } from '../context/UiContext'
+import { useContext, useEffect } from "react";
+import { UiContext } from "../context/UiContext";
 
-export const useHideMenu = ( ocultar ) => {
+export const useHideMenu = (ocultar = false) => {
+  const { showMenu, hideMenu } = useContext(UiContext);
 
-    const { showMenu, hideMenu } = useContext( UiContext );
-
-    useEffect(() => {
-        
-        if ( ocultar ) {
-            hideMenu();
-        } else {
-            showMenu()
-        }
-
-    }, [ ocultar, hideMenu, showMenu ])
-
-}
+  useEffect(() => {
+    if (ocultar) {
+      hideMenu();
+    } else {
+      showMenu();
+    }
+  }, [ocultar, hideMenu, showMenu]);
+};
