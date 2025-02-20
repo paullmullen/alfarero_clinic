9ad20101e0929ebc63aa681864c7e13e720af9c1
Paul Mullen <paullmullen@gmail.com>
2025-02-20 09:47:48 -0600

    a few updates prior to testing deployment.

--- src/helpers/fetchDaysAgo.js
+++ src/helpers/fetchDaysAgo.js
@@ -1,8 +1,6 @@
 const fetchDaysAgoData = async (daysCount) => {
-  console.log("Sending request with daysCount:", daysCount, typeof daysCount);
   const numericDaysCount =
     typeof daysCount === "number" ? daysCount : Number(daysCount);
-  console.log(typeof numericDaysCount);
   try {
     const response = await fetch(
       "https://us-central1-alfarero-478ad.cloudfunctions.net/fetchDaysAgoData",
@@ -17,7 +15,6 @@ const fetchDaysAgoData = async (daysCount) => {
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
     const data = await response.json();
-    console.log("Response:", data);
     return data;
   } catch (error) {
     console.error("Fetch error:", error);
